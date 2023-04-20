1de265aa1072056bcead5719e52529ea56fab973
Subanista <subanista14@gmail.com>
2023-04-20 16:19:17 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './layout/Navbar';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import AddRestaurant from "./restaurants/AddRestaurant";
+import EditRestaurant from './restaurants/EditRestaurant';
+import ViewRestaurant from './restaurants/ViewRestaurant';
+
+function App() { 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home/> }/>
+        <Route exact path="/AddRestaurant" element={<AddRestaurant/>}/>
+        <Route exact path="/EditRestaurant/:id" element={<EditRestaurant/>}/>
+        <Route exact path="/ViewRestaurant/:id" element={<ViewRestaurant/>}/>
+      </Routes>
+      
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { Link, useParams } from 'react-router-dom';
+
+export default function Home() {
+
+    const [restaurants, setRestaurants]=useState([]);
+
+    const { id } = useParams();
+
+    useEffect(()=>{
+    loadRestaurant();
+    },[] );
+    const loadRestaurant=async()=>{
+        const result=await axios.get("http://localhost:8080/restaurants")
+          setRestaurants(result.data);
+    }
+
+    const DeleteRestaurant = async (id) => {
+        await axios.delete(`http://localhost:8080/restaurant/${id}`);
+        loadRestaurant();
+      };
+    
+
+  return (
+    <div className='container'>
+    <div className="py-4">
+    <table className="table border shadow">
+  <thead>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Restaurant Name</th>
+      <th scope="col">Address</th>
+      <th scope="col">Email</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    {
+        restaurants.map((restaurant, index) =>(
+
+            <tr>
+                <th scope="row" key={index}>
+                    {index + 1}
+                </th>
+                <td>{restaurant.restaurantName}</td>
+                <td>{restaurant.address}</td>
+                <td>{restaurant.email}</td>
+                <td>
+                <Link
+                    className="btn btn-primary mx-2"
+                    to={`/ViewRestaurant/${restaurant.id}`}
+                  >
+                    View
+                  </Link>
+
+                    <Link className="btn btn-outline-primary mx-2"
+                     to={`/EditRestaurant/${restaurant.id}`}
+                    >Edit</Link>
+
+                    <button className="btn btn-danger mx-2"
+                    onClick={()=>DeleteRestaurant(restaurant.id)}
+                    >Delete</button>
+
+                </td>
+            </tr>
+        )
+            )
+        }
+    
+    
+  </tbody>
+</table>
+</div>
+    </div>
+  )
+}
